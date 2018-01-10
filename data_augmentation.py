30b7c181a09227bf5288626319400a72ab1eaac4
Jean Liu <jean4599@gmail.com>
2018-01-10 12:51:27 +0800

    minor modification

--- data_augmentation.py
+++ data_augmentation.py
@@ -7,10 +7,8 @@ datagen = ImageDataGenerator(rotation_range=0.2, width_shift_range=0.2, height_s
 # load img
 (train_data, train_coarse_labels), (eval_data, eval_coarse_labels) = get_data.load_data(label_mode='coarse', path='./Data/cifar-100/')
 (train_data, train_fine_labels), (eval_data, eval_fine_labels) = get_data.load_data(label_mode='fine', path='./Data/cifar-100/')
-train_fine_labels = train_fine_labels.reshape(train_fine_labels.shape[0])
-eval_fine_labels = eval_fine_labels.reshape(eval_fine_labels.shape[0])
-train_coarse_labels = train_coarse_labels.reshape(train_coarse_labels.shape[0])
-eval_coarse_labels = eval_coarse_labels.reshape(eval_coarse_labels.shape[0])
+
+print(train_coarse_labels.shape)
 
 classes = get_data.unpickle('./Data/cifar-100/meta')
 fine_class = np.asarray(classes[b'fine_label_names'], dtype=np.dtype(np.str))
@@ -25,15 +23,3 @@ for idx in range(train_data.shape[0]):
         if i == 1:
             break
 
-# img = load_img('/Desktop/lena.jpg')  # this is a PIL image, please replace to your own file path
-# x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
-# x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
-
-# the .flow() command below generates batches of randomly transformed images
-# and saves the results to the `preview/` directory
-
-# i = 0
-# for batch in datagen.flow(x, batch_size=1, save_to_dir='~/Desktop/preview', save_prefix='lena', save_format='jpg'): 
-#    i += 1
-#    if i > 20:
-#        break  # otherwise the generator would loop indefinitely
--- get_data.py
+++ get_data.py
@@ -1,11 +1,3 @@
-
-# coding: utf-8
-
-# In[54]:
-
-
-# -*- coding:utf-8 -*-
-
 import os
 import sys
 import time
@@ -87,66 +79,14 @@ def load_data(label_mode='fine', path='./Data/cifar-100'):
   fpath = os.path.join(path, 'test')
   x_test, y_test = load_batch(fpath, label_key=label_mode + '_labels')
 
-  y_train = np.reshape(y_train, (len(y_train), 1))
-  y_test = np.reshape(y_test, (len(y_test), 1))
+  y_train = np.reshape(y_train, (len(y_train)))
+  y_test = np.reshape(y_test, (len(y_test)))
 
   x_train = x_train.transpose(0, 2, 3, 1)
   x_test = x_test.transpose(0, 2, 3, 1)
 
   return (x_train, y_train), (x_test, y_test)
 
-
-# ========================================================== #
-# ├─ _random_crop()
-# ├─ _random_flip_leftright()
-# ├─ data_augmentation()
-# └─ color_preprocessing()
-# ========================================================== #
-
-def _random_crop(batch, crop_shape, padding=None):
-    oshape = np.shape(batch[0])
-
-    if padding:
-        oshape = (oshape[0] + 2 * padding, oshape[1] + 2 * padding)
-    new_batch = []
-    npad = ((padding, padding), (padding, padding), (0, 0))
-    for i in range(len(batch)):
-        new_batch.append(batch[i])
-        if padding:
-            new_batch[i] = np.lib.pad(batch[i], pad_width=npad,
-                                      mode='constant', constant_values=0)
-        nh = random.randint(0, oshape[0] - crop_shape[0])
-        nw = random.randint(0, oshape[1] - crop_shape[1])
-        new_batch[i] = new_batch[i][nh:nh + crop_shape[0],
-                       nw:nw + crop_shape[1]]
-    return new_batch
-
-
-def _random_flip_leftright(batch):
-    for i in range(len(batch)):
-        if bool(random.getrandbits(1)):
-            batch[i] = np.fliplr(batch[i])
-    return batch
-
-
-def color_preprocessing(x_train, x_test):
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train[:, :, :, 0] = (x_train[:, :, :, 0] - np.mean(x_train[:, :, :, 0])) / np.std(x_train[:, :, :, 0])
-    x_train[:, :, :, 1] = (x_train[:, :, :, 1] - np.mean(x_train[:, :, :, 1])) / np.std(x_train[:, :, :, 1])
-    x_train[:, :, :, 2] = (x_train[:, :, :, 2] - np.mean(x_train[:, :, :, 2])) / np.std(x_train[:, :, :, 2])
-
-    x_test[:, :, :, 0] = (x_test[:, :, :, 0] - np.mean(x_test[:, :, :, 0])) / np.std(x_test[:, :, :, 0])
-    x_test[:, :, :, 1] = (x_test[:, :, :, 1] - np.mean(x_test[:, :, :, 1])) / np.std(x_test[:, :, :, 1])
-    x_test[:, :, :, 2] = (x_test[:, :, :, 2] - np.mean(x_test[:, :, :, 2])) / np.std(x_test[:, :, :, 2])
-
-    return x_train, x_test
-
-
-def data_augmentation(batch):
-    batch = _random_flip_leftright(batch)
-    batch = _random_crop(batch, [32, 32], 4)
-    return np.array(batch, dtype=np.int32)
 
 def unpickle(file):
     import pickle
