838f7d990caef067caffe37b423af20624750b21
Shun-Huai Yao <shunhuai.yao@gmail.com>
2018-01-12 22:27:59 +0800

    comment devise evaluation simpleCNN

--- devise_evaluation_alltexts.py
+++ devise_evaluation_alltexts.py
@@ -76,78 +76,78 @@ print('Test Fine Label shape: ', y_test_fine_label.shape)
 print('Test Coarse Label shape:', y_test_coarse_label.shape)
 
 
-########## Evaluate DeViSE (unknown labels)##########
-# Evaluate the model and print results
-# What model to be evaluated?
-STORED_PATH = "./saved_model/cifar100_simpleCNN_devise/devise.ckpt"
+# ########## Evaluate DeViSE (unknown labels)##########
+# # Evaluate the model and print results
+# # What model to be evaluated?
+# STORED_PATH = "./saved_model/cifar100_simpleCNN_devise/devise.ckpt"
 
-# reset graph
-tf.reset_default_graph()
-tf.set_random_seed(42)
-np.random.seed(42)
+# # reset graph
+# tf.reset_default_graph()
+# tf.set_random_seed(42)
+# np.random.seed(42)
 
-print("########## Start evaluating SimpleCNN_devise ##########")
-sess = tf.Session()
-# restore the precious best model
-saver = tf.train.import_meta_graph(STORED_PATH + ".meta")
-print('operations:',tf.get_default_graph().get_operations())
-x = tf.get_default_graph().get_tensor_by_name("x:0")
-yy = tf.get_default_graph().get_tensor_by_name("y:0")
-y = tf.get_default_graph().get_tensor_by_name("y_1:0")
+# print("########## Start evaluating SimpleCNN_devise ##########")
+# sess = tf.Session()
+# # restore the precious best model
+# saver = tf.train.import_meta_graph(STORED_PATH + ".meta")
+# print('operations:',tf.get_default_graph().get_operations())
+# x = tf.get_default_graph().get_tensor_by_name("x:0")
+# yy = tf.get_default_graph().get_tensor_by_name("y:0")
+# y = tf.get_default_graph().get_tensor_by_name("y_1:0")
 
-train_mode = tf.get_default_graph().get_tensor_by_name("train_mode:0")
-visual_embeddings = tf.get_default_graph().get_tensor_by_name("transform/transform/BiasAdd:0")
-saver.restore(sess, STORED_PATH)
+# train_mode = tf.get_default_graph().get_tensor_by_name("train_mode:0")
+# visual_embeddings = tf.get_default_graph().get_tensor_by_name("transform/transform/BiasAdd:0")
+# saver.restore(sess, STORED_PATH)
 
-# train nearest neighbor model based on fine classes
-TextEmbeddings.train_nearest_neighbor(fine_classes_text_embedding, num_nearest_neighbor=5)
-
-
-# In[ ]:
+# # train nearest neighbor model based on fine classes
+# TextEmbeddings.train_nearest_neighbor(fine_classes_text_embedding, num_nearest_neighbor=5)
 
 
-top1_hit = 0.0
-top3_hit = 0.0
-top5_hit = 0.0
-for i in range(10):
-    predict_embeddings = sess.run(visual_embeddings, feed_dict={x:x_test[i*1000:(i+1)*1000],
-                                                                y:eval_fine_labels_embeddings[i*1000:(i+1)*1000],
-                                                                yy: y_test_fine_label[i*1000:(i+1)*1000],
-                                                                train_mode: False})
-    predict_batch_labels = TextEmbeddings.get_nearest_neighbor_labels(predict_embeddings, 1)
-    for idx, predict_labels in enumerate(predict_batch_labels):
-        long_true_label = FINE_CLASSES[y_test_fine_label[i*1000+idx]] # refer to class labels        
-        # consider a class name is concated by multiple labels (ex., maple_tree)
-        true_labels = long_true_label.split('_')
-        for true in true_labels:
-            if(true in predict_labels):
-                top1_hit+=1
-                print("top1 HIT!")
-    predict_batch_labels = TextEmbeddings.get_nearest_neighbor_labels(predict_embeddings, 3)
-    for idx, predict_labels in enumerate(predict_batch_labels):
-        long_true_label = FINE_CLASSES[y_test_fine_label[i*1000+idx]] # refer to class labels        
-        # consider a class name is concated by multiple labels (ex., maple_tree)
-        true_labels = long_true_label.split('_')
-        for true in true_labels:
-            if(true in predict_labels):
-                top3_hit+=1
-                print("top3 HIT!")
-    predict_batch_labels = TextEmbeddings.get_nearest_neighbor_labels(predict_embeddings, 5)
-    for idx, predict_labels in enumerate(predict_batch_labels):
-        long_true_label = FINE_CLASSES[y_test_fine_label[i*1000+idx]] # refer to class labels        
-        # consider a class name is concated by multiple labels (ex., maple_tree)
-        true_labels = long_true_label.split('_')
-        for true in true_labels:
-            if(true in predict_labels):
-                top5_hit+=1
-                print("top5 HIT!")
+# # In[ ]:
 
-print("Evaluate SimpleCNN devise")
-print("Test result: Top 1 hit rate", top1_hit/100, "%")
-print("Test result: Top 3 hit rate", top3_hit/100, "%")
-print("Test result: Top 5 hit rate", top5_hit/100, "%")
 
-sess.close()
+# top1_hit = 0.0
+# top3_hit = 0.0
+# top5_hit = 0.0
+# for i in range(10):
+#     predict_embeddings = sess.run(visual_embeddings, feed_dict={x:x_test[i*1000:(i+1)*1000],
+#                                                                 y:eval_fine_labels_embeddings[i*1000:(i+1)*1000],
+#                                                                 yy: y_test_fine_label[i*1000:(i+1)*1000],
+#                                                                 train_mode: False})
+#     predict_batch_labels = TextEmbeddings.get_nearest_neighbor_labels(predict_embeddings, 1)
+#     for idx, predict_labels in enumerate(predict_batch_labels):
+#         long_true_label = FINE_CLASSES[y_test_fine_label[i*1000+idx]] # refer to class labels        
+#         # consider a class name is concated by multiple labels (ex., maple_tree)
+#         true_labels = long_true_label.split('_')
+#         for true in true_labels:
+#             if(true in predict_labels):
+#                 top1_hit+=1
+#                 print("top1 HIT!")
+#     predict_batch_labels = TextEmbeddings.get_nearest_neighbor_labels(predict_embeddings, 3)
+#     for idx, predict_labels in enumerate(predict_batch_labels):
+#         long_true_label = FINE_CLASSES[y_test_fine_label[i*1000+idx]] # refer to class labels        
+#         # consider a class name is concated by multiple labels (ex., maple_tree)
+#         true_labels = long_true_label.split('_')
+#         for true in true_labels:
+#             if(true in predict_labels):
+#                 top3_hit+=1
+#                 print("top3 HIT!")
+#     predict_batch_labels = TextEmbeddings.get_nearest_neighbor_labels(predict_embeddings, 5)
+#     for idx, predict_labels in enumerate(predict_batch_labels):
+#         long_true_label = FINE_CLASSES[y_test_fine_label[i*1000+idx]] # refer to class labels        
+#         # consider a class name is concated by multiple labels (ex., maple_tree)
+#         true_labels = long_true_label.split('_')
+#         for true in true_labels:
+#             if(true in predict_labels):
+#                 top5_hit+=1
+#                 print("top5 HIT!")
+
+# print("Evaluate SimpleCNN devise")
+# print("Test result: Top 1 hit rate", top1_hit/100, "%")
+# print("Test result: Top 3 hit rate", top3_hit/100, "%")
+# print("Test result: Top 5 hit rate", top5_hit/100, "%")
+
+# sess.close()
 
 ########## Evaluate DeViSE (unknown labels)##########
 # Evaluate the model and print results
